9359f52017503856b635261907652c5e2f074243
MukulMairh <msoni7718@gmail.com>
2021-05-13 23:41:45 +0530

    UI 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UI1Component } from './ui1/ui1.component';
+import { UI2Component } from './ui2/ui2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UI1Component
+    UI1Component,
+    UI2Component
   ],
   imports: [
     BrowserModule
