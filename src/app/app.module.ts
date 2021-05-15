a49386d9dd6bb1f162fc4bfc78a336977c597bd3
MukulMairh <msoni7718@gmail.com>
2021-05-15 21:19:48 +0530

    Started Signup page and added material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { UI1Component } from './ui1/ui1.component';
 import { UI2Component } from './ui2/ui2.component';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     UI1Component,
-    UI2Component
+    UI2Component,
+    SignUpPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
